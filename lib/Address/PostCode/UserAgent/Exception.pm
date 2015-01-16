0d63de361f497b6cfb808f20bc0347a5580fdf6f
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-01-16 14:20:25 +0000

    - Removed SIGNATURE file. - Updated MANIFEST.SKIP file to ignore folder 'eumm/'. - Updated Copyright year information.

--- lib/Address/PostCode/UserAgent/Exception.pm
+++ lib/Address/PostCode/UserAgent/Exception.pm
@@ -1,6 +1,6 @@
 package Address::PostCode::UserAgent::Exception;
 
-$Address::PostCode::UserAgent::Exception::VERSION = '0.03';
+$Address::PostCode::UserAgent::Exception::VERSION = '0.04';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ Address::PostCode::UserAgent::Exception - Interface to exception used by Address
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
@@ -81,7 +81,7 @@ L<http://search.cpan.org/dist/Address-PostCode-UserAgent/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2014 Mohammad S Anwar.
+Copyright (C) 2014 - 2015 Mohammad S Anwar.
 
 This  program  is  free software; you can redistribute it and/or modify it under
 the  terms  of the the Artistic License (2.0). You may obtain a copy of the full
